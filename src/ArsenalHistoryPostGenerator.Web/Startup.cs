c9a6c6af73deb1a09cb49ad366db15522506e6b5
Hemang Rajyaguru <hemdagem@gmail.com>
2016-12-04 22:16:05 +0000

    updated namespaces

--- src/ArsenalHistoryPostGenerator.Web/Startup.cs
+++ src/ArsenalHistoryPostGenerator.Web/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using ArsenalHistoryPostGenerator.Domain;
 
-namespace ArsenalHistoryPostGenerator
+namespace ArsenalHistoryPostGenerator.Web
 {
     public class Startup
     {
